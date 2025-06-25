4c19dec8c10bb30708781a6d73f119dcd955940e
FernCantType <lorenzocanali@icloud.com>
2025-06-25 02:13:21 -0500

    Closed IO Objects

--- NW_SeqAllignAlg.java
+++ NW_SeqAllignAlg.java
@@ -105,5 +105,7 @@ class NW_SeqAllignAlg {
         pw.println(a1.reverse().toString());
         pw.println(a2.reverse().toString());
         pw.flush();
+        br.close()l
+        pw.close();
     }
 }
